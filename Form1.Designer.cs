ac99bf1d10adabf073042229dd2724e97f07886b
stasyandr <45917470+stasyandr@users.noreply.github.com>
2021-10-20 22:02:42 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,7 +61,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(152, 174);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Выполнить триангуляцию Делоне";
+            this.button2.Text = "Выполнить построение выпуклой оболочки методом Эндрю";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
